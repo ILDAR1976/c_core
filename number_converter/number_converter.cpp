7289c85bc98e6d216cfe726663cbe1723de98745
ILDAR1976 <akhmetov.ih@yandex.ru>
2023-05-19 08:17:21 +0300

    Adding a hexadecimal representation

--- number_converter/number_converter.cpp
+++ number_converter/number_converter.cpp
@@ -2,21 +2,29 @@
 #include <assert.h>
 #include <stdlib.h>
 
-char* convert(int);
+const char* HEXDIGITS = new char[64]{
+                       '0','1','2','3','4',
+                       '5','6','7','8','9',
+                       'A','B','C','D','E',
+                       'F' };
 
+char* convertToBin(int);
+char* convertToHex(int);
 void print(char*, int);
 
 int main()
 {
     int n = 1256789;
-    print(convert(n), 32);
+    printf("dec: %d\n", n);
+    printf("bin: "); print(convertToBin(n), 64);
+    printf("hex: "); print(convertToHex(n), 8);
     return 0;
 }
 
-char* convert(int n) {
-    int i = 32;
-    char* t = new char[32];
-    t[i] = 0;
+char* convertToBin(int n) {
+    //binary representaion
+    int i = 64;
+    char* t = new char[64];
     while (--i >= 0) {
         if ((n & 1) == 0) {
             t[i] = '0';
@@ -24,6 +32,19 @@ char* convert(int n) {
             t[i] = '1';
         }
         n >>= 1;
+    }
+    return t;
+}
+
+char* convertToHex(int n) {
+    //hex representation
+    int i = 8;
+    char* t = new char[8];
+    
+    while (--i >= 0) {
+        int d = (n & 0xf);
+        t[i] = HEXDIGITS[d];
+        n >>= 4;
     }
     return t;
 }

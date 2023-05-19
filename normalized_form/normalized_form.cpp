0a617e161bef76c94462af8a7c97edb69f006b9d
ILDAR1976 <akhmetov.ih@yandex.ru>
2023-05-19 10:04:35 +0300

    Added a project of number representation in normalized form

---
+++ normalized_form/normalized_form.cpp
@@ -0,0 +1,35 @@
+﻿#include <stdio.h>
+#include <assert.h>
+
+void normalization(double, int*, int*, double*);
+void print(char*, int);
+
+int main()
+{
+    int s = 0, e = 0;
+    double x = 100.0, m = 0.;
+    printf("dec: %lf\n", x);
+    normalization(x, &s, &e, &m);
+    printf("normalized form: %c2^%d*%lf\n", ((s==(-1))?'-':'+'),e,m);
+    return 0;
+}
+
+void normalization(double x, int* sign, int* exponent, double* mant) {
+    int s, e;double m;
+    s = 1; e = 0; m = x;
+    if (x < 0.) {
+        s = (-1); m = (-x);
+    }
+    while (m >= 2.) {
+        m /= 2.; ++e;
+        //invariant: x == s*m*2^e
+    }
+    assert(m < 2.);
+    while (m < 1.) {
+        m *= 2; --e;
+    }
+    assert(m < 2. && m >= 1);
+    *sign = s; *exponent = e; *mant = m;
+}
+
+
